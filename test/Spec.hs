01f5c8eb7d2e8cf2787dc92380f1f0e7c93d2f9e
ZhangHao <zhanghao@antigen.top>
2023-11-09 14:16:09 +0800

    Add tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+{-# LANGUAGE TemplateHaskell #-}
+
+import Test.QuickCheck.All
+import GlobalRegex
+
+prop_globmatcher1 cs =
+  case globMatcher "[!a-zA-Z0-9]" cs of
+    (Right b1) ->
+      case globMatcher "[a-zA-Z0-9]" cs of
+        (Right b2) ->
+          (and (or b1 b2) (not (and b1 b2)))
+prop_globmatcher2 cs =
+  case globMatcher "*" cs of
+    (Right b) -> b
+prop_globmatcher3 (c:[]) =
+  case globMatcher "?" [c] of
+    (Right b) -> b
+prop_globmatcher3 (c:cs) =
+  case globMatcher "?" [c] of
+    (Right b) -> (and b (prop_globmatcher3 cs))
