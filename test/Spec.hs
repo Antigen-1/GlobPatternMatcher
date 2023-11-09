54710c15d36448b0788865ef08c87a75bb9720ce
ZhangHao <zhanghao@antigen.top>
2023-11-09 17:13:28 +0800

    Fix tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,20 +1,32 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-import Test.QuickCheck.All
+import Test.QuickCheck
 import GlobRegex
 
-prop_globmatcher1 cs =
-  case globMatcher "[!a-zA-Z0-9]" cs of
+prop_globmatcher1 :: Char -> Bool
+prop_globmatcher1 c =
+  case globMatcher "[!a-zA-Z0-9]" [c] of
     (Right b1) ->
-      case globMatcher "[a-zA-Z0-9]" cs of
+      case globMatcher "[a-zA-Z0-9]" [c] of
         (Right b2) ->
-          (and (or b1 b2) (not (and b1 b2)))
+          (b1 || b2) && (not (b1 && b2))
+
+prop_globmatcher2 :: [Char] -> Bool
 prop_globmatcher2 cs =
   case globMatcher "*" cs of
     (Right b) -> b
-prop_globmatcher3 (c:[]) =
-  case globMatcher "?" [c] of
+
+help_globmatcher3 :: [Char] -> Bool
+help_globmatcher3 (cs) =
+  case globMatcher "?" cs of
     (Right b) -> b
-prop_globmatcher3 (c:cs) =
-  case globMatcher "?" [c] of
-    (Right b) -> (and b (prop_globmatcher3 cs))
+prop_globmatcher3 :: [Char] -> Bool
+prop_globmatcher3 [] = not $ help_globmatcher3 []
+prop_globmatcher3 (c:[]) = help_globmatcher3 [c]
+prop_globmatcher3 cs = not $ help_globmatcher3 cs
+
+return []
+runTests = $quickCheckAll
+
+main :: IO Bool
+main = runTests
