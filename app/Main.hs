193615124a153f081fa98b9e3fdf50b7418d2c2a
ZhangHao <zhanghao@antigen.top>
2023-11-09 13:52:41 +0800

    Add an executable

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main (main) where
 
-import Lib
+import Lib (globMatching)
+import System.Environment (getArgs)
+import System.Environment (getProgName)
 
 main :: IO ()
-main = someFunc
+main = do
+  arg <- getArgs
+  case arg of
+    (pat:[]) -> globMatching pat
+    _ -> (error "Usage: " ++ getProgName ++ " [pattern]")
